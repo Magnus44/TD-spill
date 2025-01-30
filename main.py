07d018318607da37118fce784e1a6942c87be169
OlavAase <162561583+OlavAase@users.noreply.github.com>
2025-01-30 08:44:21 +0100

    lagde vindu

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 #hovedfilen
+import pygame as pg
+from constants import *
+
+pg.init()
+clock = pg.time.Clock()
+screen = pg.display.set_mode(SIZE)
+
+
+running = True
+while running:
+
+   
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    clock.tick(FPS)
+    screen.fill(WHITE)
+
+    
+    #screen.blit(bg_image, (0, 0))
+
+    
+    pg.display.update()
+
+
+pg.quit()
